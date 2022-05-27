3b08d202d44eebce051ec58d677ace88c8c2b8ef
Alan32Liu <donggeliu@google.com>
2022-05-27 12:35:33 +1000

    A work-in-progress to trace execve of child processes

--- infra/experimental/sanitizers/ExecSan/execSan.c
+++ infra/experimental/sanitizers/ExecSan/execSan.c
@@ -15,111 +15,141 @@
 */
 /* A detector that uses ptrace to identify shell injection vulnerabilities. */
 
-#define _POSIX_C_SOURCE 200112L
+#define _POSIX_SOURCE
 
-/* C standard library */
-#include <errno.h>
-#include <stdio.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
 #include <signal.h>
-
-/* POSIX */
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/stat.h>
+
+#include <sys/ptrace.h>
+#include <sys/types.h>
 #include <sys/user.h>
 #include <sys/wait.h>
 
-/* Linux */
-#include <syscall.h>
-#include <sys/reg.h>
-#include <sys/ptrace.h>
-
-#define FATAL(...) \
-    do { \
-        fprintf(stderr, "strace: " __VA_ARGS__); \
-        fputc('\n', stderr); \
-        exit(EXIT_FAILURE); \
-    } while (0)
-
-pid_t run_child(char **argv) {
-  // Run the program under test with its args as a child process
-  pid_t pid = fork();
-  switch (pid) {
-    case -1: /* error */
-      FATAL("Fork failed: %s", strerror(errno));
-    case 0:  /* child */
-      ptrace(PTRACE_TRACEME, 0, 0, 0);
-      /* Because we're now a tracee, execvp will block until the parent
-       * attaches and allows us to continue. */
-      execv(argv[1], argv + 1);
-      FATAL("%s", strerror(errno));
-  }
-  return pid;
+void tracee_process(char **argv) {
+  /* Child */
+  printf("%d: Tracee\n", getpid());
+  ptrace(PTRACE_TRACEME, 0, 0, 0);
+  execv(argv[1], argv + 1);
+  exit(0);
 }
 
-void sync_syscall(pid_t pid) {
-  // Run and pause the child process before/after of the next system call
-  if (ptrace(PTRACE_SYSCALL, pid, NULL, NULL) == -1)
-    FATAL("%s", strerror(errno));
-  if (waitpid(pid, NULL, WUNTRACED) == -1)
-    FATAL("%s", strerror(errno));
-}
+void tracer_process(pid_t tracee_pid) {
+  /* Parent */
+  waitpid(tracee_pid, NULL, 0);
+  printf("%d: Tracer\n", getpid());
 
-void inspect(pid_t pid) {
-  // Check for the file injected upon each execve and every wait4 after clone
-  struct stat stat_buf;
-  if (!stat("/tmp/injected", &stat_buf)) {
-    ptrace(PTRACE_KILL, pid, NULL, NULL);
-    printf("===BUG DETECTED: Shell injection===\n");
-    exit(1);
+  struct user_regs_struct regs;
+  int i = 0;
+  int status = -1;
+
+  // Ensures that the tracee will never escape
+  long data = PTRACE_O_EXITKILL
+      | PTRACE_O_TRACEFORK
+      | PTRACE_O_TRACEVFORK
+      | PTRACE_O_TRACECLONE
+//      | PTRACE_O_TRACEEXEC
+      ;
+  ptrace(PTRACE_SETOPTIONS, tracee_pid, NULL, data);
+
+  while (kill(tracee_pid, 0) != -1) {
+    i++;
+    printf("=== %03d ===\n", i);
+
+    ptrace(PTRACE_SYSCALL, tracee_pid, NULL, NULL);
+    status = waitpid(tracee_pid, NULL, 0);
+
+    ptrace(PTRACE_GETREGS, tracee_pid, 0, &regs);
+    printf("%d: %lld\n", tracee_pid, regs.orig_rax);
+
+    ptrace(PTRACE_SYSCALL, tracee_pid, NULL, NULL);
+    status = waitpid(tracee_pid, NULL, 0);
+
+    pid_t child_pid = -1;
+
+    ptrace(PTRACE_GETEVENTMSG, tracee_pid, 0, &child_pid);
+    status = waitpid(child_pid, NULL, 0);
+
+    printf("%d: Child\n", child_pid);
+    printf("Exec: %d\n", status>>8 == (SIGTRAP | (PTRACE_EVENT_EXEC<<8)));
+    printf("CLONE: %d\n", status>>8 == (SIGTRAP | (PTRACE_EVENT_CLONE<<8)));
+    printf("FORK: %d\n", status>>8 == (SIGTRAP | (PTRACE_EVENT_FORK<<8)));
+    printf("VFORK: %d\n", status>>8 == (SIGTRAP | (PTRACE_EVENT_VFORK<<8)));
+    printf("WIFSTOPPED(status): %d\n", WIFSTOPPED(status));
+    printf("WSTOPSIG(status): %d\n", WSTOPSIG(status));
+
+    ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
+    ptrace(PTRACE_CONT, child_pid, 0, 0);
+    status = waitpid(child_pid, NULL, 0);
+    printf("%d: %lld\n", child_pid, regs.orig_rax);
+
+    if (regs.orig_rax == 56) {
+      printf("%d: %lld\n", child_pid, regs.orig_rax);
+      tracee_pid = child_pid; //keep the parent pid
+    }
+
+    if (regs.orig_rax == 59) {
+      printf("===BUG DETECTED: Shell injection===\n");
+    }
+
+
+//    pid_t pid2;
+//    ptrace(PTRACE_GETEVENTMSG, child_pid, 0, &pid2);
+//    printf("%d: PID2\n", pid2);
+//    printf("%d: %lld\n", pid2, regs.orig_rax);
+
+//    ptrace(PTRACE_CONT, child_pid, 0, 0);
+
+    if (status>>8 == (SIGTRAP | (PTRACE_EVENT_EXEC<<8))) {
+      ptrace(PTRACE_GETEVENTMSG, tracee_pid, 0, &child_pid);
+      ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
+      printf("%d: %lld\n", child_pid, regs.orig_rax);
+      ptrace(PTRACE_CONT, child_pid, 0, 0);
+    }
+
+    if (status>>8 == (SIGTRAP | (PTRACE_EVENT_CLONE<<8))) {
+      ptrace(PTRACE_GETEVENTMSG, tracee_pid, 0, &child_pid);
+      ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
+      printf("%d: %lld\n", child_pid, regs.orig_rax);
+      ptrace(PTRACE_CONT, child_pid, 0, 0);
+    }
+
+    if (status>>8 == (SIGTRAP | (PTRACE_EVENT_FORK<<8))) {
+      ptrace(PTRACE_GETEVENTMSG, tracee_pid, 0, &child_pid);
+      ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
+      printf("%d: %lld\n", child_pid, regs.orig_rax);
+      ptrace(PTRACE_CONT, child_pid, 0, 0);
+    }
+
+
+    if (status>>8 == (SIGTRAP | (PTRACE_EVENT_VFORK<<8))) {
+      ptrace(PTRACE_GETEVENTMSG, tracee_pid, 0, &child_pid);
+      ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
+      printf("%d: %lld\n", child_pid, regs.orig_rax);
+      ptrace(PTRACE_CONT, child_pid, 0, 0);
+    }
+
   }
 }
 
 int main(int argc, char **argv) {
-  if (argc <= 1)
-    FATAL("Expecting at least one arguments, received %d", argc - 1);
+  if (argc <= 1) {
+    printf("Expecting at least one arguments, received %d", argc - 1);
+    exit(1);
+  }
 
-  pid_t pid = run_child(argv);
-  /* Gather system call arguments */
-  struct user_regs_struct regs;
-  int cloned = 0;
-
-  /* parent */
-  // sync with child process
-  if (waitpid(pid, NULL, WUNTRACED) == -1)
-    FATAL("%s", strerror(errno));
-  // Ensures that the tracee will never escape
-  long data = PTRACE_O_EXITKILL
-//      | PTRACE_O_TRACEFORK
-//      | PTRACE_O_TRACEVFORK
-//      | PTRACE_O_TRACECLONE
-//      | PTRACE_O_TRACEEXEC
-      ;
-  ptrace(PTRACE_SETOPTIONS, pid, NULL, data);
-
-  while (kill(pid, 0) != -1) {
-    /* Enter next system call */
-    sync_syscall(pid);
-
-    /* Gather system call arguments */
-    if (ptrace(PTRACE_GETREGS, pid, 0, &regs) == -1)
-      FATAL("%s", strerror(errno));
-
-    cloned = cloned || regs.orig_rax == __NR_clone;
-
-    /* Run system call and stop on exit */
-    sync_syscall(pid);
-
-    ptrace(PTRACE_GETREGS, pid, 0, &regs);
-
-    if (regs.orig_rax == __NR_execve) {
-      inspect(pid);
-    }
-    if (regs.orig_rax == __NR_wait4 && cloned) {
-      cloned = 0;
-      inspect(pid);
-    }
+  pid_t pid = fork();
+  switch (pid) {
+    case -1:
+      printf("Failing to fork()");
+      exit(1);
+    case 0:
+      // Child
+      tracee_process(argv);
+      break;
+    default:
+      // Parent
+      tracer_process(pid);
   }
 }
